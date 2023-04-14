9e3a887ca896141607c18329bbeb7ff4e1854fb4
Aman Verma <44524984+amanverma-ism@users.noreply.github.com>
2023-04-14 12:40:55 +0530

    Add files via upload

--- ShortcutLauncher/Properties/Settings.Designer.cs
+++ ShortcutLauncher/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace ShortcutLauncher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("\"{\\\"ShortcutJsonList\\\":[]}\"")]
+        [global::System.Configuration.DefaultSettingValueAttribute("{\"ShortcutJsonList\":[]}")]
         public string ShortcutPaths {
             get {
                 return ((string)(this["ShortcutPaths"]));
